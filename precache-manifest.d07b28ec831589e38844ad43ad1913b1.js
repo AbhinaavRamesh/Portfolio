4dc8a2d989da9c988dc0ae1751541a7a21bd71f4
AbhinaavRamesh <contactabhinaav@gmail.com>
2022-09-27 17:10:11 +0000

    Updates

---
+++ precache-manifest.d07b28ec831589e38844ad43ad1913b1.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9277db44d56235a840953343f669aa83",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f1655049c1a7caf9c72c",
+    "url": "/portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8ef711ad3ae04d22b310",
+    "url": "/portfolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "b8d65ccd4a071a3476dd",
+    "url": "/portfolio/static/css/main.f451734f.chunk.css"
+  },
+  {
+    "revision": "99627d5b774b71bce6ef",
+    "url": "/portfolio/static/js/2.7e727fcb.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/portfolio/static/js/2.7e727fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1655049c1a7caf9c72c",
+    "url": "/portfolio/static/js/3.a23eb9a8.chunk.js"
+  },
+  {
+    "revision": "8ef711ad3ae04d22b310",
+    "url": "/portfolio/static/js/4.8bd388f0.chunk.js"
+  },
+  {
+    "revision": "b8d65ccd4a071a3476dd",
+    "url": "/portfolio/static/js/main.4a505d83.chunk.js"
+  },
+  {
+    "revision": "ca2b60a8b95b13df3841",
+    "url": "/portfolio/static/js/runtime-main.68b56159.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "6fe646df2acd4f72a96e53049dec365e",
+    "url": "/portfolio/static/media/DeepLearningAI.6fe646df.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f6b99467ba468d56644fac511ffa12ae",
+    "url": "/portfolio/static/media/arubaLogo.f6b99467.png"
+  },
+  {
+    "revision": "47eddfa9c1346bea1a2b7fd525256d65",
+    "url": "/portfolio/static/media/boschLogo.47eddfa9.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5a0f065fe282309dee5ec0e75ac1fc3f",
+    "url": "/portfolio/static/media/googleDataAnalytics.5a0f065f.png"
+  },
+  {
+    "revision": "c34e90cc76d3fb4ecfc99039b2433427",
+    "url": "/portfolio/static/media/iniLogo.c34e90cc.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3c4111f6a4bafa8c7829fe70a8bdd5c0",
+    "url": "/portfolio/static/media/psgLogo.3c4111f6.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "69a339974b645659ac6ea58965d02be6",
+    "url": "/portfolio/static/media/stanfordLogo.69a33997.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "75d4e624739abbe6f36552f28b34c326",
+    "url": "/portfolio/static/media/ucsdLogo.75d4e624.svg"
+  },
+  {
+    "revision": "98fb8b2a45f0652aafc467c3e9fe3ebf",
+    "url": "/portfolio/static/media/uscLogo.98fb8b2a.png"
+  }
+]);
